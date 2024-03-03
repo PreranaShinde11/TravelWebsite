902927ea5806b6d2ccd0fa447964b152a706fea6
PreranaShinde11 <109781382+PreranaShinde11@users.noreply.github.com>
2024-03-03 22:08:49 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,28 @@
+<?php
+
+$fullname = $_POST['fullname'];
+$email = $_POST['email'];
+$gender = $_POST['gender'];
+$destination = $_POST['destination'];
+#$date= date('Y-m-d',strtotime($_POST['date']));
+$ppl = $_POST['ppl'];
+$number = $_POST['number'];
+$budget = $_POST['budget'];
+
+//database connection
+ $conn = new mysqli('localhost','root','','customer');
+ if($conn->connect_error){
+ 	die('Connection Failed: ' .$conn->connect_error);
+
+ }
+ else
+ {
+ 	$stmt = $conn->prepare("insert into registration(fullname, email, gender, destination, ppl, number, budget) VALUES (?, ?, ?, ?, ?, ?, ?)");
+ 	$stmt->bind_param("sssssii",$fullname, $email, $gender, $destination, $ppl, $number, $budget);
+ 	$stmt->execute();
+ 	echo "registration Successful";
+ 	$stmt->close();
+ 	$conn->close();
+
+ }
+?>
